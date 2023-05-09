4889672a526f97200c8db60efb97f70c5d0a6bef
Muddabirchohan <muddabir22@gmail.com>
2023-05-10 00:26:31 +0500

    fix

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -12,11 +12,11 @@ import About from "../components/About/about";
 import App from "../App";
 
 const RouterDefault = () => (
-  <Router>
+  <Router basename="/react-enterprise-architecture">
     <Routes>
     {/* <Route path="/api/v1/app/login" element={<MainSection/>} /> */}
 
-      <Route path="/react-enterprise-architecture" element={<PageLayout />}>
+      <Route path="/" element={<PageLayout />}>
       <Route path="/" element={<App/>} />
 
         <Route path="/api/v1/about" element={<About/>} />
